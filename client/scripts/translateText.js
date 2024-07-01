df6658ebd279aa489f1fe2fcf7d92fca59072a97
Glitchyi <advaithnarayanan8@gmail.com>
2024-07-01 15:39:50 +0530

    yeyey

--- client/scripts/translateText.js
+++ client/scripts/translateText.js
@@ -1,34 +1,24 @@
-const apiKey = 'AIzaSyA_h4ANWNkvWtfGn2_dg3LCPJN3L7CccJ8'
-
-// async function translateText(inputText) {
-//     try {
-//         fetch('https://translation.googleapis.com/language/translate/v2', 
-//             method: 'POST',
-//             {
-//             q: inputText,
-//             target: 'en', // Change this to the target language code you want to translate to
-//             key: apiKey
-//         });
-
-//         return response.data.data.translations[0].translatedText;
-//     } catch (error) {
-//         console.error('Error translating text:', error);
-//         throw error;
-//     }
-// }
+const apiKey = 'AIzaSyASJvnbHLrtr_w6XjU_qTcLErXKtTk33g4'
 
 async function translateText(inputText) {
-    await fetch("https://translation.googleapis.com/language/translate/v2", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            q: inputText,
-            target: 'en',
-            key: apiKey
-        }),
-    }).then(response => console.log(response.json()));
+    try {
+        const response = await fetch("https://translation.googleapis.com/language/translate/v2", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                q: inputText,
+                target: 'en',
+                key: apiKey
+            }),
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error translating text:', error);
+    }
 }
+
 
 translateText('നമസ്കാരം'); // Output: Hello
